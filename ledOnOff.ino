aa62674317e705502ac271fbf20e4cd4e3c4ba14
Edgar Jose Garcia Landaeta <edgar.jose.garcia.l@gmail.com>
2023-07-26 17:02:48 -0500

    First commit

---
+++ ledOnOff.ino
@@ -0,0 +1,13 @@
+void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(2,OUTPUT);
+}
+
+void loop() {
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+     digitalWrite(2,LOW);   
+    delay(750);                        // wait for a second
+    digitalWrite(LED_BUILTIN, LOW); 
+     digitalWrite(2,HIGH);             // turn the LED off by making the voltage LOW
+    delay(750);                        // wait for a second
+}
