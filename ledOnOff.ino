8cdb6a74b3d7f2d9deb93fa0f8b17d4954e966a5
Edgar Jose Garcia Landaeta <edgar.jose.garcia.l@gmail.com>
2023-07-26 17:06:42 -0500

    Refactoring

--- ledOnOff.ino
+++ ledOnOff.ino
@@ -5,9 +5,9 @@ void setup() {
 
 void loop() {
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-     digitalWrite(2,LOW);   
-    delay(750);                        // wait for a second
+    digitalWrite(2,LOW);   
+    delay(750);                        // wait for a time
     digitalWrite(LED_BUILTIN, LOW); 
-     digitalWrite(2,HIGH);             // turn the LED off by making the voltage LOW
-    delay(750);                        // wait for a second
+    digitalWrite(2,HIGH);             // turn the LED off by making the voltage LOW
+    delay(750);                        // wait for a time
 }
