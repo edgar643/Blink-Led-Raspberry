99470704979b6ace51957ddff0f20f9ffb8eae3f
Edgar Jose Garcia Landaeta <edgar.jose.garcia.l@gmail.com>
2023-07-28 23:27:00 -0500

    Shift register on row led

--- ledOnOff.ino
+++ ledOnOff.ino
@@ -1,13 +1,20 @@
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(0, OUTPUT);
+    pinMode(1,OUTPUT);
     pinMode(2,OUTPUT);
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    digitalWrite(0, HIGH);   // turn the LED on (HIGH is the voltage level)
+    digitalWrite(1,LOW); 
     digitalWrite(2,LOW);   
-    delay(750);                        // wait for a time
-    digitalWrite(LED_BUILTIN, LOW); 
-    digitalWrite(2,HIGH);             // turn the LED off by making the voltage LOW
-    delay(750);                        // wait for a time
+    delay(750);             // wait for a time
+    digitalWrite(0, LOW);   
+    digitalWrite(1,HIGH); 
+    digitalWrite(2,LOW);   
+    delay(750);            // wait for a time
+    digitalWrite(0, LOW);
+    digitalWrite(1,LOW); 
+    digitalWrite(2,HIGH);   
+    delay(750);            // wait for a time
 }
