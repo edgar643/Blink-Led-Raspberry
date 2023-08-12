daf769febeacd5fe954d0bbe4f1c00b594ba6425
Edgar José <edgar.jose.garcia.l@gmail.com>
2023-08-12 09:01:37 -0500

    Another secuence created

--- ledOnOff.ino
+++ ledOnOff.ino
@@ -1,20 +1,59 @@
 void setup() {
+ 
     pinMode(0, OUTPUT);
     pinMode(1,OUTPUT);
     pinMode(2,OUTPUT);
+    pinMode(3,OUTPUT);
 }
 
 void loop() {
+  int xDelay = 100;
     digitalWrite(0, HIGH);   // turn the LED on (HIGH is the voltage level)
-    digitalWrite(1,LOW); 
-    digitalWrite(2,LOW);   
-    delay(750);             // wait for a time
+    digitalWrite(1,HIGH); 
+    digitalWrite(2,HIGH);  
+    digitalWrite(3,HIGH); 
+    delay(xDelay);             // wait for a time
     digitalWrite(0, LOW);   
     digitalWrite(1,HIGH); 
-    digitalWrite(2,LOW);   
-    delay(750);            // wait for a time
+    digitalWrite(2,HIGH);  
+    digitalWrite(3,HIGH);  
+   delay(xDelay);              // wait for a time
     digitalWrite(0, LOW);
     digitalWrite(1,LOW); 
-    digitalWrite(2,HIGH);   
-    delay(750);            // wait for a time
+    digitalWrite(2,HIGH);  
+    digitalWrite(3,HIGH);  
+   delay(xDelay);            // wait for a time
+    digitalWrite(0, LOW);
+    digitalWrite(1,LOW); 
+    digitalWrite(2,LOW);  
+    digitalWrite(3,HIGH);  
+delay(xDelay);   
+    digitalWrite(0, LOW);
+    digitalWrite(1,LOW); 
+    digitalWrite(2,LOW);  
+    digitalWrite(3,LOW);  
+    delay(xDelay);   
+     digitalWrite(0,HIGH);
+    digitalWrite(1,LOW); 
+    digitalWrite(2,LOW);  
+    digitalWrite(3,LOW);  
+   delay(xDelay); 
+    digitalWrite(0,HIGH);
+    digitalWrite(1,HIGH); 
+    digitalWrite(2,LOW);  
+    digitalWrite(3,LOW);  
+ delay(xDelay);  
+    digitalWrite(0,HIGH);
+    digitalWrite(1,HIGH); 
+    digitalWrite(2,HIGH);  
+    digitalWrite(3,LOW);  
+  delay(xDelay);  
+digitalWrite(0,HIGH);
+    digitalWrite(1,HIGH); 
+    digitalWrite(2,HIGH);  
+    digitalWrite(3,LOW);  
+delay(xDelay);  
+
+
+
 }
